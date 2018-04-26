9bdf8fb4ed80e3e0a71e801ccfb0793e1d592493
Justin Head <justin_head@ymail.com>
2018-04-26 13:07:53 -0500

    Finished all methods

--- client/app.js
+++ client/app.js
@@ -1,19 +1,66 @@
 const url = '/chirps';
 
 
-// Chirp get method
+// Chirp with get, put, and delete method
 
 let findChirps = $.getJSON( "api/chirps", function(msgs) {
     let keys = Object.keys(msgs);
      for (let i of keys) { 
+
         $(`<tr class='text-center' id='row${i}'></tr>`).appendTo('#display');
-        $(`<td>${msgs[i].text}</td>`).appendTo(`#row${i}`);
+        $(`<td id='col${i}' data-toggle="modal" data-target="#myModal">${msgs[i].text}</td><td class='eraseRow${i}'>X</td>`).appendTo(`#row${i}`);
+
+        $(`#col${i}`).click(function(){
+
+            $('.modal-body').html(`<textarea id="t${i}">${msgs[i].text}</textarea>`);
+            $('.modal-footer').html(`<button id="s${i}" type="button" class="btn btn-default" data-dismiss="modal">Save Changes</button>`);
+
+            $(`#s${i}`).click(function(){
+                let message = $(`#t${i}`).val()
+                    console.log(message);
+                let textChange = {
+                        text: `${message}`
+                    };
+                $.ajax ({
+                    url: `api/chirps/${i}`,
+                    type: "PUT",
+                    data: JSON.stringify(textChange),
+                    contentType: "application/json; charset=utf-8",
+                }).done(() => {
+                    console.log('success');
+                    window.location.reload(true);
+                }).fail((err) => {
+                    console.log('failed');
+                    console.log(err);
+                });
+
+            })
+             
+        })    
+
+        $(`.eraseRow${i}`).click(function() {
+            console.log(`.eraseRow${i}`);
+            $.ajax({
+                url: `api/chirps/${i}`,
+                method: 'DELETE',
+                contentType: 'application/json',
+            }).done(() => {
+                console.log('success');
+                $(`#row${i}`).remove();
+            }).fail((err) => {
+                console.log(err);
+        
+            });
+        
+        });
         if (`#row${i}` == `#rownextid`) {
             $('#rownextid').remove();
         }
 
     }
 });
+
+
 
 // Chirp post method
 
@@ -39,4 +86,3 @@ let inputs = {
   
 
 
- 
